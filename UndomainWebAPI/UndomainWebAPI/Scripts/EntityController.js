7e38be3c6fb415dd2a44b438486e6ed9c396ae97
Jake Waletzko <jnwaletzko@gmail.com>
2014-05-31 15:03:19 -0500

    Connected UI to DB through Web API

--- UndomainWebAPI/UndomainWebAPI/Scripts/EntityController.js
+++ UndomainWebAPI/UndomainWebAPI/Scripts/EntityController.js
@@ -1,5 +1,21 @@
-﻿app.controller("EntityController", function ($scope) {
-    $scope.entitities = [
-        {}
-    ]
+﻿undomainApp.controller("EntityController", function ($scope, entityFactory) {
+    $scope.entities = [];
+
+    init();
+
+    function init() {
+        getEntities();
+    }
+
+    function getEntities() {
+        entityFactory.query(function (response) {
+            $scope.entities = response;
+        })
+    };
+
+    $scope.addEntity = function () {
+        entityFactory.save($scope.newEntity, function () {
+            getEntities();
+        });
+    };
 });
--- UndomainWebAPI/UndomainWebAPI/Scripts/undomainApp.js
+++ UndomainWebAPI/UndomainWebAPI/Scripts/undomainApp.js
@@ -1,16 +1,69 @@
-﻿var app = angular.module('UndomainApp', []);
+﻿var undomainApp = angular.module('undomainApp', ['ngResource', 'ngRoute']);
 
-app.config(function ($routeProvider) {
+
+undomainApp.config(function ($routeProvider) {
     $routeProvider
-        .when('/',
+        .when('/view1',
             {
                 controller: 'EntityController',
                 templateUrl: 'Partials/View1.html'
             })
-        .when('/partial2',
+        .when('/view2',
                 {
                     controller: 'EntityController',
                     templateUrl: 'Partials/View2.html'
                 })
-        .otherwise({ redirectTo: '/' });
+        .otherwise({ redirectTo: '/view1' });
 });
+
+
+undomainApp.factory('entityFactory', function ($resource) {
+    return $resource('/api/entity', {});
+});
+
+//undomainApp.factory('entityFactory', ['$http', function($http) {
+
+//    var urlBase = '/api/entity'
+//    var factory = {};
+
+//    factory.getEntities = function () {
+//        return $http.get(urlBase);
+//    };
+
+//    factory.getEntity = function (id) {
+//        return $http.get(urlBase + '/' + id);
+//    }
+
+//    factory.insertEntity = function (entity) {
+//        return $http.post(urlBase, entity)
+//    };
+
+//    factory.updateEntity = function (entity) {
+//        return $http.put(urlBase + '/' + entity.id, entity)
+//    };
+
+//    factory.deleteEntity = function (id) {
+//        return $http.delete(urlBase + '/' + id)
+//    };
+
+//    return factory;
+//}]);
+
+
+//undomainApp.factory('simpleFactory', function () {
+//    var entities = [
+//           {    id: 1, valueFamily: "Customer", value: "C347186", type: 0, parentID: 0 },
+//           {    id: 2, valueFamily: "Name", value: "Acme Corp", type: 1, parentID: 1 },
+//           {    id: 3, valueFamily: "Address Type", value: "Corp HQ", type: 1, parentID: 1 },
+//           {    id: 4, valueFamily: "Address Line 1", value: "1234 Walnut Ave", type: 1, parentID: 1 }
+//    ];
+
+//    var factory = {};
+//    factory.getEntities = function () {
+//        return entities;
+//    };
+//    factory.postEntity = function (entity) {
+//    };
+
+//    return factory;
+//});
